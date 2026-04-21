25b56244a7b28b0990ce6244e076e649a647a81a
mohammad-alashhab <mohammad.alashhab19@gmail.com>
2026-04-21 17:12:48 +0300

    Add favicon & global style css, with some edit on other files

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../features/home/pages/HomePage";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
